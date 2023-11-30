5363591cf4b948ae921c1d466c1ed3b51892a2b8
19khaled88 <kkhaled88hasan@gmail.com>
2023-12-01 00:27:58 +0600

    compiled graphql server ts code to js

---
+++ build/index.js
@@ -0,0 +1,59 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = require("@apollo/server");
+const express4_1 = require("@apollo/server/express4");
+const body_parser_1 = __importDefault(require("body-parser"));
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const typeDefs = `#graphql
+
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
+const books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster',
+    },
+];
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
+function startServer() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const app = (0, express_1.default)();
+        const server = new server_1.ApolloServer({
+            typeDefs,
+            resolvers
+        });
+        app.use(body_parser_1.default.json());
+        app.use((0, cors_1.default)());
+        yield server.start();
+        app.use('/graphql', (0, express4_1.expressMiddleware)(server));
+        app.listen(8001, () => console.log('Server started on http://localhost:8001/graphql'));
+    });
+}
+startServer();
