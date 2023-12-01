52bdd977d55dedf36dc4dc495705be771a5b02fb
19khaled88 <kkhaled88hasan@gmail.com>
2023-12-01 11:47:39 +0600

    install jsonwebtoken & bcrypt library

---
+++ src/app/utils/jwtValidation.ts
@@ -0,0 +1,26 @@
+import jwt, {JwtPayload,Secret} from 'jsonwebtoken'
+import config from '../config'
+
+
+const createToken =async(id:number)=>{
+
+   return jwt.sign({ userId: id }, config.jwt_secret as Secret, { expiresIn: '1d' })
+  
+}
+
+const getInfoFromToken =async(token:string)=>{
+   
+   try {
+     
+      const userData = jwt.verify(token,config.jwt_secret as Secret) as { userId:number}
+      
+      return userData
+   } catch (error) {
+      return error
+   }
+}
+
+export const jwtHelper ={
+   createToken,
+   getInfoFromToken
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import {ApolloServerPluginDrainHttpServer} from '@apollo/server/plugin/drainHttpServer'
 import {expressMiddleware} from '@apollo/server/express4'
+import {PrismaClient,Prisma} from '@prisma/client'
+import {DefaultArgs} from '@prisma/client/runtime/library'
 import bodyParser from 'body-parser';
 import express from 'express'
+import http from 'http'
 import cors from 'cors'
+
+
+const app = express()
+const httpServer = http.createServer(app)
+
+
+interface Context {
+  prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>
+  // userInfo: {
+  //   userId: number
+  // } 
+}
+
+interface MyContext {
+  token?: String;
+}
+
+
 
 const typeDefs = `#graphql
 
@@ -26,25 +48,32 @@ const books = [
     },
   ];
 
-  const resolvers = {
+const resolvers = {
     Query: {
       books: () => books,
     },
   };
 
   async function startServer(){
-    const app = express()
+    
     const server = new ApolloServer({
       typeDefs,
-      resolvers
+      resolvers,
+      plugins:[ApolloServerPluginDrainHttpServer({httpServer})],
+      introspection:true
     })
-
     app.use(bodyParser.json())
     app.use(cors())
 
     await server.start()
 
-    app.use('/graphql',expressMiddleware(server))
+    app.use(
+      '/graphql',
+
+      cors<cors.CorsRequest>(),
+      express.json(),
+      expressMiddleware(server)
+      )
 
     app.listen(8001,()=>console.log('Server started on http://localhost:8001/graphql'))
   }
