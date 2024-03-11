63ed7320539ffd6f6c7861dfe95e286d93b84015
19khaled88 <kkhaled88hasan@gmail.com>
2024-03-11 13:52:54 +0600

    change database

--- prisma/migrations/20231130175903_book_migrate/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "books" (
-    "bookId" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "author" TEXT NOT NULL,
-
-    CONSTRAINT "books_pkey" PRIMARY KEY ("bookId")
-);
--- prisma/migrations/20231217154914_added_new_field_to_post_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `short_name` to the `posts` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "posts" ADD COLUMN     "short_name" TEXT NOT NULL;
